20de11437cf653e408d383c6cecd36b3096f87c1
Borislav111 <btraychev@gmail.com>
2023-05-09 01:30:44 +0300

    feat(footer): add automated test case for checking if 'Sign Up' button is the right color

---
+++ src/test/java/tests/testCaseFour.java
@@ -0,0 +1,17 @@
+package tests;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class testCaseFour extends BaseTest{
+    @Test
+    public void signUpBtnColor(){
+        HomePage homePage = new HomePage(driver);
+
+        homePage.navigateUrl();
+        String signUpColorBtn = homePage.getFooterSignUpColorBtn();
+        Assert.assertEquals(signUpColorBtn, "rgb(255,82,79)", "The 'Sign Up' background color mismatches");
+    }
+}
